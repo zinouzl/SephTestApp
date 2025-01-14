a29e66566d9d38b28398148339a19191d59e6a97
Zine Eddine <latiou_z@modulotech.fr>
2025-01-14 08:45:59 +0100

    feature: move main activity to presentation

--- presentation/src/main/java/io/seph/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/io/seph/presentation/ui/MainActivity.kt
@@ -1,16 +1,30 @@
-package io.seph.test
+package io.seph.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import io.seph.presentation.R
+import io.seph.presentation.databinding.ActivityMainBinding
 
+/**
+ * The main activity of the application.
+ *
+ * This activity serves as the entry point for the application and is responsible for
+ * setting up the user interface.
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
